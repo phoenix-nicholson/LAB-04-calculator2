c557d30accfd4a45e1c72f501290b23fb8b97b03
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2021-09-30 14:31:53 -0700

    got full addition calculator running properly

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
+import { add } from './calculations.js';
 
 const addInput1 = document.getElementById('add-input-1');
 const addInput2 = document.getElementById('add-input-2');
-const addOutput = document.getElementById('add-answer');
+const addAnswer = document.getElementById('add-answer');
 const addButton = document.getElementById('add-btn');
-console.log(addInput1, addInput2, addOutput, addButton);
+console.log(addInput1, addInput2, addAnswer, addButton);
 
 addButton.addEventListener('click', ()=> {
-  console.log("add button is getting clicked");
-});
+    const value1 = Number(addInput1.value);
+    const value2 = Number(addInput2.value);
+    const result = add(value1, value2);
+
+    addAnswer.textContent = result;
+}); 
 
 
 
